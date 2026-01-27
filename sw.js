27f5647bc8267ccd0aa0dce903a4cac380946264
almax-es <luisoscar.soler@gmail.com>
2026-01-27 21:36:53 +0100

    Deploy v20260127-203653: Actualizacion cache y mejoras

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 // IMPORTANTE: Al hacer deploy, actualiza CACHE_VERSION con la fecha/hora actual para forzar actualización.
 // Bump this on every deploy to force clients to pick up the latest precache.
-const CACHE_VERSION = "20260127-201843";
+const CACHE_VERSION = "20260127-203653";
 const CACHE_NAME = `luzfija-static-${CACHE_VERSION}`;
 
 
