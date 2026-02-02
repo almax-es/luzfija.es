1e64437b0f636d9d5d0287e5207bfdca8d33f732
almax-es <luisoscar.soler@gmail.com>
2026-02-02 17:34:56 +0100

    Fix mobile CSV table layout in observatorio

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 // IMPORTANTE: Al hacer deploy, actualiza CACHE_VERSION con la fecha/hora actual para forzar actualización.
 // Bump this on every deploy to force clients to pick up the latest precache.
-const CACHE_VERSION = "20260203-041410";
+const CACHE_VERSION = "20260203-053620";
 const CACHE_NAME = `luzfija-static-${CACHE_VERSION}`;
 
 
