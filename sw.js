de67bb9d020516d8c7f9ab7aafb7c1845a41aacd
almax-es <luisoscar.soler@gmail.com>
2026-02-02 14:56:26 +0100

    Expand observatorio charts on wide screens

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 // IMPORTANTE: Al hacer deploy, actualiza CACHE_VERSION con la fecha/hora actual para forzar actualización.
 // Bump this on every deploy to force clients to pick up the latest precache.
-const CACHE_VERSION = "20260202-174010";
+const CACHE_VERSION = "20260202-175740";
 const CACHE_NAME = `luzfija-static-${CACHE_VERSION}`;
 
 
